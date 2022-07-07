2ae6574fa9f28605ca95d1b095c2506799d1e4ba
Debbyoruta <deborahoruta@gmail.com>
2022-07-06 17:23:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/*
-	 * string initialization
-	 */
-	char Mystr[100] = "Programming is like building a multilingual puzzle";
-
-	puts(Mystr);
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

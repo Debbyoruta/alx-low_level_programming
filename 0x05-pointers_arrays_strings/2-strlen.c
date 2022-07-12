afd082154f00f2af034cb42d8805b44bb92d6b60
Debbyoruta <deborahoruta@gmail.com>
2022-07-12 16:21:43 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * _strlen - Function that returns the length of the string
+ * @s: This is the input string
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	return (0);
+}
